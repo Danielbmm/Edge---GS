6639bafd86b45d23ac8a5ad199512a816cf12485
Felipe <felipevoidela@gmail.com>
2023-04-24 18:51:14 -0300

    criacao de arvore de arquivos

---
+++ OneDrive/Área de Trabalho/CP-2/quiz.js
@@ -0,0 +1,15 @@
+let test=confirm("Vamos conhecer sobre vinho")
+
+let pergunta1 =(prompt("doq é feito o vinho?"))
+
+if (pergunta1 === "uva"){
+      document.write("A resposta está correta pois é ", pergunta1)
+}else{
+      document.write("A resposta está errada, a resposta correta seria uva")
+}
+
+
+
+
+
+
